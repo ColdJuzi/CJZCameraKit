ec96b80d59df0649418fb48e0768971c3d347a30
LiangHao <lianghao@megvii.com>
2021-08-18 14:46:27 +0800

    add - change classas and header API with pods.

---
+++ Classes/Exclude/Exclude.md
@@ -0,0 +1,2 @@
+# CJZCameraKit
+iOS 摄像头和视频相关操作
---
+++ Classes/ProHeader/CJZCameraProConfig.h
@@ -0,0 +1,12 @@
+//
+//  CJZCameraProConfig.h
+//  CJZCameraKit
+//
+//  Created by Liang Hao on 2021/8/18.
+//
+
+#ifndef CJZCameraProConfig_h
+#define CJZCameraProConfig_h
+
+
+#endif /* CJZCameraProConfig_h */
